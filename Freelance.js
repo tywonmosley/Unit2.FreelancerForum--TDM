08a9d62277b6259c080d1a958f73317fcbe52be3
tywonmosley <146139910+tywonmosley@users.noreply.github.com>
2023-11-21 22:09:14 -0500

    update files

--- Freelance.js
+++ Freelance.js
@@ -1,1 +1,15 @@
-console.log("hi")
+
+//Establish freelancer array
+const freelancer = [
+    {name: "Alice", price: 30, occupcation: "writer"},
+    {name: "Bob", price: 50, occupcation: "teacher"},
+    {name: "Carol", price: 70, occupcation: "programmerr"},
+    ];
+console.log(freelancer)
+
+function price() {
+    
+}
+
+
+// const initFree = document.querySelector()
